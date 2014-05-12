9ab44942024ae67b6921202c8edffb4625ddb151
Alexander Ji <alexji@mit.edu>
2014-05-12 17:22:12 -0400

    New cleaner version of compute_mmix_grid.py

--- compute_mmix_grid.py
+++ compute_mmix_grid.py
@@ -6,65 +6,64 @@ from tophat import TopHat
 
 import h5py
 
-if __name__=="__main__":
-    #tmin = 0.0; tmax = 1000.0; dt = 0.1
-    #VMIX = karlsson.Vmix_K05
-    #RHO = karlsson.rho_K05A
+def run_compute_mmix_grid(filename,Mhalo,vturb,lturb,tmax,dt,tmin=0.0):
+    """
+    Assume a TopHat density function
+    Mhalo: Msun
+    vturb: km/s
+    lturb: kpc
+    tmax, dt: Myr
+    """
+    vturb *= 3.16/3.08 * .001 #km/s to kpc/yr
+    Dt =  vturb * lturb / 3.0
+    print filename
+    print "Mhalo",Mhalo,"vturb",vturb,"lturb",lturb,"Dt",Dt
+    print "tmax",tmax,"dt",dt
 
-    #FILENAME = "TESTa_Mmixgrid_K05_tmax"+str(tmax)+"_dt"+str(dt)
-    #print "starting compute_mmix_grid (may take a long time)"
-    #start = time.time()
-    #tarr,tauarr,Mmixgrid = karlsson.calc_Mmix_grid(VMIXDOT,RHO,
-    #    tmin=tmin,tmax=tmax,dt=dt,numprocs=1)
-    #np.savez(FILENAME,tarr=tarr,tauarr=tauarr,Mmixgrid=Mmixgrid)
-    #print "compute_mmix_grid time:",time.time()-start
-    
-    #FILENAME = "TESTb_Mmixgrid_K05_tmax"+str(tmax)+"_dt"+str(dt)
-    #print "starting compute_mmix_grid_fast (may take a long time)"
-    #start = time.time()
-    #tarr,tauarr,Mmixgrid = karlsson.calc_Mmix_grid_fast(VMIX,RHO,
-    #    tmin=tmin,tmax=tmax,dt=dt)
-    #np.savez(FILENAME,tarr=tarr,tauarr=tauarr,Mmixgrid=Mmixgrid)
-    #print "compute_mmix_grid_fast time:",time.time()-start
-
-    #FILENAME = "TESTc_Mmixgrid_K05_tmax"+str(tmax)+"_dt"+str(dt)
-    #print "starting compute_mmix_grid_fast_trap (may take a long time)"
-    #start = time.time()
-    #tarr,tauarr,Mmixgrid = karlsson.calc_Mmix_grid_fast_trap(VMIX,RHO,
-    #    tmin=tmin,tmax=tmax,dt=dt)
-    #np.savez(FILENAME,tarr=tarr,tauarr=tauarr,Mmixgrid=Mmixgrid)
-    #print "compute_mmix_grid_fast_trap time:",time.time()-start
-
-    #tmin = 0.0; tmax = 1000.0; dt = 0.1
-    #th = TopHat()
-    #RHO = th.get_rho_of_t_fn()
-    #VMIX = karlsson.get_Vmixfn_K08(RHO)
-    #FILENAME = "Mmixgrid_K08_tmax"+str(tmax)+"_dt"+str(dt)
-    #print "computing K08 with midpoint (may take a long time)"
-    #start = time.time()
-    #tarr,tauarr,Mmixgrid = karlsson.calc_Mmix_grid_fast(VMIX,RHO,
-    #    tmin=tmin,tmax=tmax,dt=dt)
-    #np.savez(FILENAME,tarr=tarr,tauarr=tauarr,Mmixgrid=Mmixgrid)
-    #print "compute_mmix_grid_fast time:",time.time()-start
-
-    tmin = 0.0; tmax = 100.0; dt = 0.03
-    th = TopHat()
+    th = TopHat(Mhalo=Mhalo,nvir=0.1,fb=0.1551,mu=1.4)
     RHO = th.get_rho_of_t_fn()
-    VMIX = karlsson.get_Vmixfn_K08(RHO)
-    FILENAME = "Mmixgrid_K08_tmax"+str(tmax)+"_dt"+str(dt)
+    VMIX = karlsson.get_Vmixfn_K08(RHO,Dt=Dt)
+    FILENAME = filename#+"Mmixgrid_K08_tmax"+str(tmax)+"_dt"+str(dt)
+
     f = h5py.File(FILENAME+'.hdf5',"w")
-    print "computing K08 hires with midpoint (may take a long time)"
+    print "computing mmixgrid with midpoint (may take a long time)"
     start = time.time()
     tarr,tauarr,Mmixgrid = karlsson.calc_Mmix_grid_fast(VMIX,RHO,
         tmin=tmin,tmax=tmax,dt=dt)
-    print "compute_mmix_grid_fast time:",time.time()-start
+    print "calc_Mmix_grid_fast time:",time.time()-start
     f.attrs['tmin'] = tmin
     f.attrs['tmax'] = tmax
     f.attrs['dt'] = dt
     f.attrs['rho'] = 'tophat'
-    f.attrs['vmix'] = 'K08'
+    f.attrs['vmix'] = 'K08_Dt'+str(Dt)
+    f.attrs['Mhalo'] = Mhalo
+
     f['tarr']=tarr
     f['tauarr']=tauarr
     f['Mmixgrid']=Mmixgrid
     f.close()
-    #np.savez(FILENAME,tarr=tarr,tauarr=tauarr,Mmixgrid=Mmixgrid)
+
+
+if __name__=="__main__":
+    ## Compute Minihalo
+    filename='mmixgrid_minihalo'
+    Mhalo = 10.**6
+    vturb = 2. #km/s
+    lturb = .01 #kpc
+    tmax = 100. #Myr
+    dt = 0.01
+    run_compute_mmix_grid(filename,Mhalo,vturb,lturb,tmax,dt)
+
+    ## Compute Fat Minihalo
+    filename='mmixgrid_fatminihalo'
+
+    ## Compute Atomic Cooling Halo
+    filename='mmixgrid_atomiccoolinghalo'
+    Mhalo = 10.**8
+    vturb = 10. #km/s
+    lturb = .1 #kpc
+    tmax = 1000. #Myr
+    dt = 0.1
+    run_compute_mmix_grid(filename,Mhalo,vturb,lturb,tmax,dt)
+
+    
